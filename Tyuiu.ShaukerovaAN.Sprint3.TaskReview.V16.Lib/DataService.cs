94e076cf89920eb1b649d9f191fc05e99e6c928d
ShaukerovaAdeliya <Tyuiu.ShaukerovaAN@outlook.com>
2023-11-18 13:34:10 +0500

    TaskReview

--- Tyuiu.ShaukerovaAN.Sprint3.TaskReview.V16.Lib/DataService.cs
+++ Tyuiu.ShaukerovaAN.Sprint3.TaskReview.V16.Lib/DataService.cs
@@ -4,9 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.ShaukerovaAN.Sprint3.TaskReview.V16.Lib
 {
-    public class DataService
+    public class DataService : ISprint3Task7V16
     {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((Math.Cos(x) / (x - 0.4)) + Math.Sin(x) * 8 * x + 2, 2);
+                valueArray[count] = y;
+                count++;
+            }
+            return valueArray;
+        }
     }
 }
--- Tyuiu.ShaukerovaAN.Sprint3.TaskReview.V16/Program.cs
+++ Tyuiu.ShaukerovaAN.Sprint3.TaskReview.V16/Program.cs
@@ -4,12 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Tyuiu.ShaukerovaAN.Sprint3.TaskReview.V16.Lib;
+
 namespace Tyuiu.ShaukerovaAN.Sprint3.TaskReview.V16
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Шаукерова А.Н. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Шаукерова Аделия Нуржановна | ИСТНб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу,которая выводит таблицу значений функции             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("|    X     |    f(x)   |");
+            Console.WriteLine("+----------+-----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |   {1,6:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+-----------+");
+            Console.ReadKey();
         }
     }
 }
